75c0306566954a406f7fae042fd0641c092a8a70
Arti2911 <139011882+Arti2911@users.noreply.github.com>
2025-01-28 22:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById("form1");
+	const first= form.elements["fname"].value;
+	const last=form.elements["lname"].value;
+	alert(`${first} ${last}`);
 
 }
